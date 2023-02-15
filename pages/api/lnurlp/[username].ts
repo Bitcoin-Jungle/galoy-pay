81660615765fd01a6d28d6221c5775c2cc8003dc
Lee Salminen <leesalminen@gmail.com>
2023-02-15 06:47:36 -0600

    store nostr zap request note in redis, is too big for invoice description

--- pages/api/lnurlp/[username].ts
+++ pages/api/lnurlp/[username].ts
@@ -9,6 +9,7 @@ import {
   InMemoryCache,
 } from "@apollo/client"
 import type { NextApiRequest, NextApiResponse } from "next"
+import Redis from "ioredis"
 
 import { GRAPHQL_URI_INTERNAL, NOSTR_PUBKEY } from "../../../lib/config"
 
@@ -41,26 +42,50 @@ const USER_WALLET_ID = gql`
 `
 
 const LNURL_INVOICE = gql`
-  mutation lnInvoiceCreateOnBehalfOfRecipient($walletId: WalletId!, $amount: SatAmount!, $memo: Memo!) {
+  mutation lnInvoiceCreateOnBehalfOfRecipient($walletId: WalletId!, $amount: SatAmount!, $memo: Memo, $descriptionHash: Hex32Bytes) {
     mutationData: lnInvoiceCreateOnBehalfOfRecipient(
-      input: { recipientWalletId: $walletId, amount: $amount, memo: $memo }
+      input: { recipientWalletId: $walletId, amount: $amount, memo: $memo, descriptionHash: $descriptionHash }
     ) {
       errors {
         message
       }
       invoice {
+        paymentHash
         paymentRequest
       }
     }
   }
 `
 
+const connectionObj = {
+  sentinelPassword: process.env.REDIS_PASSWORD,
+  sentinels: [
+    {
+      host: `${process.env.REDIS_0_DNS}`,
+      port: process.env.REDIS_0_SENTINEL_PORT || 26379,
+    },
+    {
+      host: `${process.env.REDIS_1_DNS}`,
+      port: process.env.REDIS_1_SENTINEL_PORT || 26379,
+    },
+    {
+      host: `${process.env.REDIS_2_DNS}`,
+      port: process.env.REDIS_2_SENTINEL_PORT || 26379,
+    },
+  ],
+  name: process.env.REDIS_MASTER_NAME ?? "mymaster",
+  password: process.env.REDIS_PASSWORD,
+}
+
+//@ts-ignore
+const redis = new Redis(connectionObj)
+
+redis.on("error", (err) => console.log({ err }, "Redis error"))
+
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const { username, amount, nostr, comment } = req.query
   const url = originalUrl(req)
   const userAgent = req.headers['user-agent']
-
-  console.log({ headers: req.headers }, "request to NextApiRequest")
 
   let walletId
 
@@ -116,14 +141,18 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         }
       }
 
-      let description = ""
+      let mutationVariables: {[k: string]: any} = {
+        walletId,
+        amount: amountSats,
+        descriptionHash: null,
+        memo: null,
+      }
 
       if(nostr) {
-        // @ts-ignore
-        description = nostr
+        //@ts-ignore
+        mutationVariables.descriptionHash = crypto.createHash("sha256").update(nostr).digest("hex")
       } else if(comment) {
-        // @ts-ignore
-        description = comment
+        mutationVariables.memo = comment
       }
 
       const {
@@ -132,11 +161,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         },
       } = await client.mutate({
         mutation: LNURL_INVOICE,
-        variables: {
-          walletId,
-          amount: amountSats,
-          memo: description,
-        },
+        variables: mutationVariables
       })
 
       if (errors && errors.length) {
@@ -145,6 +170,11 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
           status: "ERROR",
           reason: `Failed to get invoice: ${errors[0].message}`,
         })
+      }
+
+      if(nostr) {
+        //@ts-ignore
+        redis.set(`nostrInvoice:${invoice.paymentHash}`, nostr, "EX", 600)
       }
 
       res.json({
