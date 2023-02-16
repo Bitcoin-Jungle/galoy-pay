8c1ff3529410e0719e8c2d4bdb43a891139a9751
Lee Salminen <leesalminen@gmail.com>
2023-02-16 14:29:18 -0600

    need cors headers for nostr

--- next.config.js
+++ next.config.js
@@ -4,4 +4,21 @@ module.exports = {
       { source: "/.well-known/lnurlp/:username", destination: "/api/lnurlp/:username" },
     ]
   },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/lnurlp/:username',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'OPTIONS,GET',
+          }
+        ],
+      },
+    ]
+  },
 }
